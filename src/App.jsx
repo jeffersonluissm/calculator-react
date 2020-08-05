647371dc082f3e098e6657710ad2590298b388f5
Jefferson Mariano <jeffersonluiss2003@gmail.com>
2020-08-04 21:26:00 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Calculator from './main/Calculator'
+
+function App() {
+    return ( 
+        <div>
+            <h1>Calculadora</h1>
+            <Calculator />
+        </div>
+    );
+}
+
+export default App;
